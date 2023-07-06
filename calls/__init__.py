a89a25bd7e4516fc54306151a81e186c13ed92c2
zbabers <t-zbabers@microsoft.com>
2023-07-06 14:54:01 -0500

    Added dev branch, small print change.

--- calls/__init__.py
+++ calls/__init__.py
@@ -8,7 +8,6 @@ response = (requests.get("https://" + ip + "/redfish/v1/Systems/1", auth=(userna
 
 if response.status_code == 200:
     print("Response Successful")
-    print(response.text)
 else:
     print("response unsuccessful with status code: ", response.status_code)
 
