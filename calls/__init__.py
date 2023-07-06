c17c2991f563a8708cf2c28991aee6bf9f3ef930
zbabers <t-zbabers@microsoft.com>
2023-07-06 17:07:50 -0500

    testing getting name

--- calls/__init__.py
+++ calls/__init__.py
@@ -1,7 +1,7 @@
 import requests
 import warnings
 from .functions import basic_request
-from .functions import get_memory_info
+from .functions import get_model_name
 
 ip = input("Enter iLO IP: ")
 username = input("Enter iLO Username: ")
@@ -14,6 +14,6 @@ if response.status_code == 200:
 else:
     print("response unsuccessful with status code: ", response.status_code)
 
-get_memory_info(ip, username, password)
+get_model_name(ip, username, password)
 
 # get all the information
