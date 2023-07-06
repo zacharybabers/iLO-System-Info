18853ddd15b135d5e7451bde4d54d2c7ef2c11cc
zbabers <t-zbabers@microsoft.com>
2023-07-06 15:14:33 -0500

    slight fix

--- calls/functions.py
+++ calls/functions.py
@@ -2,7 +2,7 @@ import warnings
 import requests
 
 def basic_request(ip, username, password, redfish_item):
-    with warnings.catch_warnings:
+    with warnings.catch_warnings():
         warnings.simplefilter("ignore")
         response = (requests.get("https://" + ip + "/redfish/v1/" + redfish_item, auth=(username, password), verify=False))
     return response
