f828af87c5abc6bfd409e675b067e94572083f63
zbabers <t-zbabers@microsoft.com>
2023-07-06 15:09:32 -0500

    abstract out request function call

--- calls/__init__.py
+++ calls/__init__.py
@@ -1,13 +1,12 @@
 import requests
 import warnings
-
-warnings.filterwarnings("ignore", message="InsecureRequestWarning: Unverified HTTPS request is being made. Adding certificate verification is strongly advised. See: https://urllib3.readthedocs.io/en/latest/advanced-usage.html")
+from .functions import functions
 
 ip = input("enter redfish ip")
 username = input("enter username")
 password = input("enter password")
 
-response = (requests.get("https://" + ip + "/redfish/v1/Systems/1", auth=(username, password), verify=False))
+response = functions.basic_request(ip, username, password, "Systems/1")
 
 if response.status_code == 200:
     print("Response Successful")
