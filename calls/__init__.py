db1ed0b83c3d353b5cfd571480d12b56403c7bb6
zbabers <t-zbabers@microsoft.com>
2023-07-06 16:04:46 -0500

    small amendment

--- calls/__init__.py
+++ calls/__init__.py
@@ -7,7 +7,7 @@ ip = input("Enter iLO IP: ")
 username = input("Enter iLO Username: ")
 password = input("Enter iLO Password: ")
 
-response = basic_request(ip, username, password, "redfish/v1/Systems")
+response = basic_request(ip, username, password, "/redfish/v1/Systems")
 
 if response.status_code == 200:
     print("Response Successful")
