965db3a140d4a1ccf7306f740a46afa5eb6b1fb3
zbabers <t-zbabers@microsoft.com>
2023-07-06 15:50:31 -0500

    removed test output

--- calls/__init__.py
+++ calls/__init__.py
@@ -11,7 +11,6 @@ response = basic_request(ip, username, password, "Systems")
 
 if response.status_code == 200:
     print("Response Successful")
-    print(response.text)
 else:
     print("response unsuccessful with status code: ", response.status_code)
 
