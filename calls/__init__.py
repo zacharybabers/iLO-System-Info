4ab88f9b80425fb7c49d33003140f0a9d03fc50f
zbabers <t-zbabers@microsoft.com>
2023-07-10 12:29:11 -0500

    get network interface count

--- calls/__init__.py
+++ calls/__init__.py
@@ -1,9 +1,7 @@
 import requests
 import warnings
 from .functions import basic_request
-from .functions import get_drive_objects
-from .functions import get_storage_objects
-from .functions import print_drive_info
+from .functions import get_network_interface_count
 
 ip = input("Enter iLO IP: ")
 username = input("Enter iLO Username: ")
@@ -16,8 +14,6 @@ if response.status_code == 200:
 else:
     print("response unsuccessful with status code: ", response.status_code)
 
-for storage in get_storage_objects(ip, username, password):
-    for drive in get_drive_objects(ip, username, password, storage):
-        print_drive_info(drive)
+print(get_network_interface_count(ip, username, password))
 
 # get all the information
--- calls/functions.py
+++ calls/functions.py
@@ -112,4 +112,10 @@ def print_drive_info(drive):
     infoString += "Physical Location: " + drive['PhysicalLocation']['PartLocation']['ServiceLabel'] + "\n"
     infoString += "Ordinal Location: " + str(drive['PhysicalLocation']['PartLocation']['LocationOrdinalValue']) + "\n"
     print(infoString)
+
+# Network Interface functions; there are currently no network interfaces being returned by our iLO, so I will just display the number of network interfaces so it is apparent if/when this functionality needs to be added
+def get_network_interface_count(ip, username, password):
+    interfaces = json.loads(basic_request(ip, username, password, "/redfish/v1/Systems/1/NetworkInterfaces"),text)
+    return interfaces['Members@odata.count']
+
     
