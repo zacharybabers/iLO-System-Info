1fbcda100d3713f4c470de200b2d64cd460da2e7
zbabers <t-zbabers@microsoft.com>
2023-07-10 10:04:09 -0500

    test process id again

--- calls/functions.py
+++ calls/functions.py
@@ -55,5 +55,5 @@ def get_processorIDs(ip, username, password):
     systems = get_system_objects(ip, username, password)
     for system in systems:
         for processor in system['Processors']:
-            processorIDs.append(processor)
+            processorIDs.append(type(processor))
     return processorIDs
