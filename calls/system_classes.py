b34a76b78f33cb7b9d123d3d6329d63ded70cd54
zbabers <t-zbabers@microsoft.com>
2023-07-20 11:33:18 -0500

    test class

---
+++ calls/system_classes.py
@@ -0,0 +1,9 @@
+
+class MemoryInfo:
+    def __init__(self):
+        self.info1 = 0;
+        self.info2 = 0;
+
+    def introduce(self):
+        print("hi my name is what? my name is who? my name is wikikwikwiw slim shady")
+
