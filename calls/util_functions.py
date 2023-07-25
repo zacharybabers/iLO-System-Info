d8989484a650c5cc04ef344d330ea56e32b096d1
zbabers <t-zbabers@microsoft.com>
2023-07-25 12:06:08 -0500

    debug

--- calls/util_functions.py
+++ calls/util_functions.py
@@ -75,7 +75,7 @@ def get_sys_rows(computerSystem):
     if maxNumRows > 1:
         for i in range(1, maxNumRows):
             extraRow = []
-            for i in range(0, 7):
+            for j in range(0, 7):
                 extraRow.append('-')
             if len(computerSystem.networkAdapterList) > i:
                 extraRow.append(computerSystem.networkAdapterList[i].name)
