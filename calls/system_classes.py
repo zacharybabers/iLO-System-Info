a7600afc65c5cf37bf52d5a49999049b9df90247
zbabers <t-zbabers@microsoft.com>
2023-07-26 12:17:01 -0500

    debug

--- calls/system_classes.py
+++ calls/system_classes.py
@@ -200,7 +200,7 @@ def populate_system(ip, username, password):
         drives.append(get_drive_objects(ip, username, password, storage))
     driveInfos = []
     for drive in drives:
-        print(drive)
+        print(type(drive))
         driveInfos.append(DriveInfo(drive))
 
     # NICs
