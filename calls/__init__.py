bc1e8e0e856e9eac56eeb886dcd1194e323439d8
zbabers <t-zbabers@microsoft.com>
2023-07-31 16:59:01 -0500

    time it

--- calls/__init__.py
+++ calls/__init__.py
@@ -1,6 +1,7 @@
 import getpass
 import sys
 import pandas as pd
+import time
 from .util_functions import get_ips
 from .util_functions import process_file
 from .util_functions import df_list
@@ -47,7 +48,13 @@ print("\n")
 
 servers = []
 for ip in ipList:
+    startTime = time.time()
     servers.append(populate_system(ip, username, password))
+    endTime = time.time()
+
+    executionTime = endTime - startTime
+    
+    print("Populated information for server at ip " + ip + " in " + executionTime)
 
 if printMode == "table":
     lst = build_list(servers)
